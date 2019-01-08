fd0915411d81f452a34180e6b3e95a349299db1f
GardenLee <41745717+97e57e@users.noreply.github.com>
2019-01-08 17:13:49 +0900

    Create MyToast.java

---
+++ DAY19/MyToast.java
@@ -0,0 +1,56 @@
+/*
+
+토스트와 스낵바 만들기.
+
+toastborder.xml
+toast.xml을 새로 만든다
+
+*/
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),"위치가 바뀐 토스트",Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP|Gravity.LEFT,200, 200);
+                toast.show();
+
+            }
+        });
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toastborder, (ViewGroup)findViewById(R.id.toast_layout_root));
+
+                TextView text = (TextView) layout.findViewById(R.id.text);
+                text.setText("모양을 바꾼 토스트");
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER,0,-100);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+
+                toast.show();
+            }
+        });
+
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "스낵바 입니다.", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
