0be734643867f4f0a36e7ed0fdaaf449596d651b
GardenLee <41745717+97e57e@users.noreply.github.com>
2019-01-21 19:38:35 +0900

    Create MySpinner.java

---
+++ DAY25/MySpinner.java
@@ -0,0 +1,32 @@
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    String[] items= {"학생1", "학생2", "학생3", "학생4"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(items[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                textView.setText("선택 :  ");
+            }
+        });
+    }
+}
